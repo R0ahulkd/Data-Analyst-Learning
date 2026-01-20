568a0d739938d42063f3fca1855e3f6326f04858
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-01-20 21:20:31 +0530

    Module Creation

---
+++ 01_python/08_Modules/01_Module_Creation/01_create_module.py
@@ -0,0 +1,9 @@
+# import demo1
+
+# a = demo1.add(3,5)
+# print(a)
+
+import demo1 as d
+
+a = d.add(3,5)
+print(a)
---
+++ 01_python/08_Modules/01_Module_Creation/02_Create_module.py
@@ -0,0 +1,7 @@
+import demo2 as d
+
+a = d.add(3,5)
+print(a)
+
+b = d.employee["Name"]
+print(b)
