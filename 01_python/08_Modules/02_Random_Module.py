e4f1f86ad9d68da7c70c7cc43df596ec55061a56
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-01-20 17:15:37 +0530

    Random and Math Module

---
+++ 01_python/08_Modules/02_Random_Module.py
@@ -0,0 +1,9 @@
+import random
+
+a = random.randint(0,50)
+print(a)
+
+print(random.random())
+
+l = ["Head","Tailes"]
+print(random.choice(l))
---
+++ 01_python/08_Modules/03_Math_Module.py
@@ -0,0 +1,16 @@
+import math
+
+x = max(25,63,21,45,85)
+print(x)
+
+x = min(25,63,21,45,85)
+print(x)
+
+print(pow(2,4))
+print(math.sqrt(49))
+
+print(abs(-12.36))
+print(abs(-12.36 * 4))
+
+print(math.floor(3.999)) # Floor gives 3 down whole number
+print(math.ceil(3.01)) # Ceil gives 4 Up whole number
