b1c355df5b04e4c4e73b3d8f86b8b323217ec59a
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-02-18 18:59:06 +0530

    Between with order by

---
+++ 04_MYSQL/07_Between_Operator/02_Between_Operator.sql
@@ -0,0 +1,3 @@
+select * from employee_data.employee_sample_data;
+
+select * from employee_data.employee_sample_data where Annual_Salary between "$1,00,000" and "$1,20,000" order by Annual_Salary desc;
