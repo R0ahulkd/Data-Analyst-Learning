b0e66e68b3e5915d8c325701fd2437b5ec8eab43
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-02-19 15:51:44 +0530

    IN and NOT IN Operator

---
+++ 04_MYSQL/08_In_Notin_Operator/03_Notin_Operator.sql
@@ -0,0 +1,3 @@
+select * from employee_data.employee_sample_data;
+
+select * from employee_data.employee_sample_data where City not in ("Seattle","Miami");
---
+++ 04_MYSQL/08_In_Notin_Operator/04_In_And_Operator.sql
@@ -0,0 +1,3 @@
+select * from employee_data.employee_sample_data;
+
+select * from employee_data.employee_sample_data where City in ("Seattle","Miami") and Department in ("IT","Accounting");
