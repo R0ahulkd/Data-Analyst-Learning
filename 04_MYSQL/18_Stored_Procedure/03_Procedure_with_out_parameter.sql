502209667ca9dc7447f0f54a1a6bd48503072d13
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-02-23 18:30:34 +0530

    Procedure with in and out parameters

---
+++ 04_MYSQL/18_Stored_Procedure/03_Procedure_with_out_parameter.sql
@@ -0,0 +1,11 @@
+DELIMITER &&
+
+CREATE PROCEDURE get_credit(out var int)
+BEGIN
+    SELECT max(creditLimit) into var FROM classicmodels.customers;
+END &&
+
+DELIMITER ;
+
+call classicmodels.get_credit(@m);
+select @M #@m is just a variable name
